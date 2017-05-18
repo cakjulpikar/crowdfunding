2df01908181a2c6a993452634781b9236ffae5aa
aldyadk <aldy.andika@gmail.com>
2017-05-18 16:58:44 +0700

    done bikin partial view

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ var router = express.Router();
 var db = require('../models')
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
-//Testing associations 1
-router.get('/',function (req,res,next) {
-  db.
-})
+// Testing associations 1
+// router.get('/',function (req,res,next) {
+//   db.
+// })
 
 module.exports = router;
